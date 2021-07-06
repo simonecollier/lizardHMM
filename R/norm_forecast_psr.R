d9e500154d821d86be41f83f0bba0b9ab1aa511b
Simone Collier <simonekcollier@gmail.com>
2021-07-06 15:23:37 -0700

    norm_forecast_psr

---
+++ R/norm_forecast_psr.R
@@ -0,0 +1,54 @@
+norm_forecast_psr <- function(x, hmm, state_dep_dist_pooled = FALSE) {
+  #' Compute forecast normal pseuso-residuals
+  #'
+  #' This function computes the normal forecast pseudo-residuals of the data `x`
+  #'   fitted with `hmm`.
+  #'
+  #' @inheritParams norm_viterbi
+  #'
+  #' @return A list of vectors (one for each subject) of the pseudo-residuals.
+  #' @export
+
+  n             <- nrow(x)
+  num_states    <- hmm$num_states
+  num_variables <- hmm$num_variables
+  num_subjects  <- hmm$num_subjects
+  lalpha        <- norm_logforward(x, hmm,
+                                   state_dep_dist_pooled)
+  forecast_psr   <- list()
+  for (i in 1:num_subjects) {
+    s_ind   <- i
+    if (state_dep_dist_pooled) {
+      s_ind <- 1
+    }
+    pstepmat          <- matrix(NA, n, num_states)
+    forecast_psr[[i]] <- rep(NA, n)
+    ind.step          <- 1:n
+    for (j in 1:num_variables) {
+      ind.step <- sort(intersect(ind.step, which(!is.na(x[, j, i]))))
+    }
+    for (k in ind.step) {
+      for (m in 1:num_states) {
+        P   <- 1
+        for (j in 1:num_variables) {
+          P <- P*stats::pnorm(x[k, j, i], mean = hmm$mu[[j]][s_ind, m],
+                              sd = hmm$sigma[[j]][s_ind, m])
+        }
+        pstepmat[k, m] <- P
+      }
+    }
+    if (1 %in% ind.step) {
+      forecast_psr[[i]][1] <- stats::qnorm(hmm$delta[[i]] %*% pstepmat[1, ])
+    }
+    for (t in 2:n) {
+      c <- max(lalpha[[i]][, t - 1])
+      a <- exp(lalpha[[i]][, t - 1] - c)
+      if (t %in% ind.step) {
+        forecast_psr[[i]][t] <- stats::qnorm(t(a) %*%
+                                               (hmm$gamma[[i]][[t]]/sum(a)) %*%
+                                               pstepmat[t, ])
+      }
+    }
+  }
+  forecast_psr
+}
