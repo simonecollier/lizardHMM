f7c654d812be4da95063aecb9ae08b3113689280
Simone Collier <simonekcollier@gmail.com>
2021-07-05 17:59:41 -0700

    update

--- R/norm_working_params.R
+++ R/norm_working_params.R
@@ -2,7 +2,7 @@ norm_working_params <- function(num_states, num_variables, num_subjects,
                                 mu, sigma, beta, delta) {
   #' Transform normal HMM parameters from natural to working
   #'
-  #' This function transforms the natural normal HMM parameters that have
+  #' The `norm_working_params` function transforms the natural normal HMM parameters that have
   #' additional constraints into working parameters that incorporate the
   #' constraints. The output is a single vector which includes all the working
   #' parameters.
@@ -10,7 +10,7 @@ norm_working_params <- function(num_states, num_variables, num_subjects,
   #' @param num_states The number of states in the HMM.
   #' @param num_subjects The number of subjects that generated the data being
   #'   fit with the HMM.
-  #' @param num_subjects The number of variables in the HMM data.
+  #' @param num_variables The number of variables in the HMM data.
   #' @param mu The means for the normally distributed state dependent
   #'   distributions of the HMM. mu is a list of matrices with each matrix
   #'   corresponding to a different variable in the data being fit. The columns
@@ -21,12 +21,10 @@ norm_working_params <- function(num_states, num_variables, num_subjects,
   #'   each matrix corresponding to a different variable in the data being fit.
   #'   The columns of the matrices correspond to the state number and the rows
   #'   correspond to the subject number.
-  #' @param gamma The matrix of regression coefficients for the covariates
+  #' @param beta The matrix of regression coefficients for the covariates
   #'   included in the estimated of the transition probability matrix.
   #' @param delta A list with each element being the initial state distribution
   #'   vector of the HMM for the subject corresponding to that index.
-  #' @param stationary A logical variable denoting whether to treat the HMM as
-  #'   one with a stationary distribution or without.
 
   tmu     <- numeric()
   tsigma  <- numeric()
