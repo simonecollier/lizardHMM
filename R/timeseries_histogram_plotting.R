df0726abf893ef9556526bc19dbeb0e32d7c5227
Simone Collier <simonekcollier@gmail.com>
2021-07-13 14:50:14 -0700

    add norm_hist function and change timeserires file name

---
+++ R/timeseries_histogram_plotting.R
@@ -0,0 +1,105 @@
+#' Plot timeseries of observations
+#'
+#' @inheritParams norm_fit_hmm
+#' @param states A matrix with the columns containing the sequence of states
+#' that generated the data in `x` for the given subject.
+#' @param variable_names A vector containing the names of the variables in the
+#'   data `x`.
+#' @param subject_names A vector containing the names of the subjects generating
+#'   the data `x`.
+#' @param length A number indicating the desired length of the timeseries plot.
+#'
+#' @return A grid of plots of the time series for each subject and variable.
+#' @export
+#' @importFrom ggplot2 ggplot aes ggtitle theme labs geom_point geom_line
+
+timeseries_plot <- function(x, states, num_subjects, num_variables,
+                            variable_names = c("Var 1", "Var 2", "Var 3"),
+                            subject_names = c("Subject 1", "Subject 2",
+                                              "Subject 3", "Subject 4"),
+                            length = 300) {
+  n      <- nrow(x)
+  plots  <- list()
+  for (i in 1:num_subjects) {
+    data       <- data.frame('State' = as.factor(states[1:length, i]))
+    data$Time  <- 1:length
+    for (j in 1:num_variables) {
+      data$Observation <- x[1:length, j, i]
+      p <- ggplot(data, ggplot2::aes(x = Time, y = Observation)) +
+        ggplot2::theme_light() +
+        ggtitle(subject_names[i]) +
+        theme(axis.title.x = ggplot2::element_blank(),
+              plot.title = ggplot2::element_text(hjust = 0.5)) +
+        labs(x = '', y = variable_names[j]) +
+        geom_point(ggplot2::aes(color = State)) +
+        geom_line(colour = 'grey', alpha = 0.8, lwd = 0.4)
+      plots <- c(plots, list(p))
+    }
+  }
+  plots
+  #egg::ggarrange(plotlist = plots, common.legend = TRUE, legend = "bottom")
+}
+
+
+#' Title
+#'
+#' @param sample
+#' @param num_states
+#' @param num_subjects
+#' @param num_variables
+#' @param hmm
+#' @param width
+#' @param x_step
+#'
+#' @return
+#' @export
+#' @importFrom ggplot2 ggplot aes ggtitle theme_bw labs geom_line
+#' @importFrom stats dnorm
+#'
+#' @examples
+norm_hist <- function(sample, num_states, num_subjects, num_variables,
+                      hmm, width = 1, x_step = 0.2) {
+  states <- sample$state
+  x      <- sample$observ
+  n      <- nrow(x)
+  Var    <- c("Variable 1", "Variable 2", "Variable 3", "Variable 4")
+  Sub    <- c("Subject 1", "Subject 2", "Subject 3", "Subject 4")
+  plots  <- list()
+  for (i in 1:num_subjects) {
+    subvar_data <- data.frame('State' = as.factor(states[, i]))
+    for (j in 1:num_variables) {
+      subvar_data$Observation <- x[, j, i]
+      h <- ggplot() +
+        geom_histogram(data = subvar_data,
+                       aes(x = Observation),
+                       binwidth = width,
+                       colour = "cornsilk4",
+                       fill = "white") +
+        theme_bw() +
+        ggtitle(Sub[i]) +
+        theme(panel.grid.major = ggplot2::element_blank(),
+              panel.grid.minor = ggplot2::element_blank(),
+              plot.title = ggplot2::element_text(hjust = 0.5)) +
+        labs(x = Var[j], y = '')
+
+      xfit <- seq(min(subvar_data$Observation), max(subvar_data$Observation),
+                  by = x_step)
+      marginal <- numeric(length(xfit))
+      for (k in 1:num_states) {
+        yfit     <- dnorm(xfit, hmm$mu[[j]][i, k], hmm$sigma[[j]][i, k])
+        yfit     <- yfit*sum(subvar_data$State == k)*width
+        df       <- data.frame('xfit' = xfit, 'yfit' = yfit,
+                               col = as.factor(rep(k, length(xfit))))
+        h        <- h + geom_line(data = df, aes(xfit, yfit, colour = col),
+                                  lwd = 0.7)
+        marginal <- marginal + yfit
+      }
+      h     <- h + labs(color = "State")
+      df    <- data.frame('xfit' = xfit, 'yfit' = marginal)
+      h     <- h + geom_line(data = df, aes(xfit, yfit), col="black", lwd=0.7)
+      plots <- c(plots, list(h))
+    }
+  }
+  plots
+  #ggarrange(plotlist = plots, common.legend = TRUE, legend = "bottom")
+}
