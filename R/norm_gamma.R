52a2b6677990b358a9e9417cd2b464029d9db5f3
Simone Collier <simonekcollier@gmail.com>
2021-07-13 16:13:57 -0700

    norm_gamma roxygen edit

--- R/norm_gamma.R
+++ R/norm_gamma.R
@@ -5,16 +5,37 @@
 #' coefficients `beta`.
 #'
 #' @param num_states The number of states in the desired HMM.
-#' @param num_subjects The number of subjects that generated the data.
+#' @param num_subjects The number of subjects/trials that generated the data.
 #' @param num_time A value indicating the length of the data.
-#' @param beta A matrix of regression coefficients.
+#' @param beta A matrix of regression coefficients for the effect of the
+#'   covariates on the transition probability matrices `gamma`.
 #' @param design A list of design matrices, one for each subject which
 #' indicate the values of the each of the covariates (column) at each point
 #' in time (row).
 #'
-#' @return A matrix.
+#' @return A list containing a list for each subject/trial of the transition
+#'   probability matrices at each time step.
 #' @export
-
+#' @example
+#' # define parameters
+#' num_states   <- 3
+#' num_subjects <- 1
+#' num_time     <- 1000
+#' beta             <- matrix(c(0.01, 0.02, 0.001,
+#'                              0.01, 0.03, 0.004,
+#'                              0.01, 0.01, 0.003,
+#'                              0.01, 0.04, 0.002,
+#'                              0.01, 0.01, 0.004,
+#'                              0.01, 0.03, 0.001),
+#'                              ncol = 3, nrow = 6, byrow = TRUE)
+#' design           <- list(matrix(0, nrow = 1000, ncol = 3))
+#' design[[1]][, 1] <- 1 # First column is the intercept
+#' design[[1]][, 2] <- sample(c(0, 1), size = 1000,
+#'                            prob = c(0.3, 0.7), replace = TRUE)
+#' design[[1]][, 3] <- rnorm(1000, mean = 5, sd = 1)
+#'
+#' # compute the transition probability matrices
+#' norm_gamma(num_states, num_subjects, num_time, beta, design)
 norm_gamma <- function(num_states, num_subjects, num_time, beta, design) {
   gamma <- list()
   for (i in 1:num_subjects) {
