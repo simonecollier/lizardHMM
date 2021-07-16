45a671b8918676b6ed648d0906d4e24b66fecec0
Simone Collier <simonekcollier@gmail.com>
2021-07-16 13:50:50 -0700

    fix gam_natural_params

--- R/gam_natural_params.R
+++ R/gam_natural_params.R
@@ -17,7 +17,7 @@
 #'
 #' @return A list of the natural paramters.
 #' @export
-norm_natural_params <- function(num_states, num_variables, num_subjects,
+gam_natural_params <- function(num_states, num_variables, num_subjects,
                                 num_covariates, working_params,
                                 state_dep_dist_pooled = FALSE) {
   ns             <- num_subjects
