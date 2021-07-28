cdbbce3a224e3e0d61474eac832b5fb5f66c2f99
Simone Collier <simonekcollier@gmail.com>
2021-07-28 16:12:34 -0400

    fit_tpm: gamma[[i]] -> array...

--- R/fit_tpm.R
+++ R/fit_tpm.R
@@ -6,6 +6,8 @@
 #'
 #' @param num_states The number of states in the desired HMM.
 #' @param num_subjects The number of subjects/trials that generated the data.
+#' @param num_covariates The number of covariates in the data that the
+#'   transition probability matrix depends on.
 #' @param num_time A value indicating the length of the data.
 #' @param beta A matrix of regression coefficients for the effect of the
 #'   covariates on the transition probability matrices `gamma`.
@@ -16,34 +18,18 @@
 #' @return A list containing a list for each subject/trial of the transition
 #'   probability matrices at each time step.
 #' @export
-#' @examples
-#' # define parameters
-#' num_states   <- 3
-#' num_subjects <- 1
-#' num_time     <- 1000
-#' beta             <- matrix(c(0.01, 0.02, 0.001,
-#'                              0.01, 0.03, 0.004,
-#'                              0.01, 0.01, 0.003,
-#'                              0.01, 0.04, 0.002,
-#'                              0.01, 0.01, 0.004,
-#'                              0.01, 0.03, 0.001),
-#'                              ncol = 3, nrow = 6, byrow = TRUE)
-#' design           <- list(matrix(0, nrow = 1000, ncol = 3))
-#' design[[1]][, 1] <- 1 # First column is the intercept
-#' design[[1]][, 2] <- sample(c(0, 1), size = 1000,
-#'                            prob = c(0.3, 0.7), replace = TRUE)
-#' design[[1]][, 3] <- rnorm(1000, mean = 5, sd = 1)
-#'
-#' # compute the transition probability matrices
-#' fit_tpm(num_states, num_subjects, num_time, beta, design)
-fit_tpm <- function(num_states, num_subjects, num_time, beta, design) {
+fit_tpm <- function(num_states, num_subjects, num_covariates, num_time,
+                    beta, design) {
   gamma <- list()
+  if (num_covariates == 0) {
+    num_time <- 1
+  }
   for (i in 1:num_subjects) {
-    gamma[[i]] <- list()
+    gamma[[i]] <- array(dim = c(num_states, num_states, num_time))
     for (t in 1:num_time) {
-      eta             <- matrix(0, nrow = num_states, ncol = num_states)
-      gamma[[i]][[t]] <- matrix(0, nrow = num_states, ncol = num_states)
-      betarow         <- 1
+      eta               <- matrix(0, nrow = num_states, ncol = num_states)
+      gamma[[i]][, , t] <- matrix(0, nrow = num_states, ncol = num_states)
+      betarow           <- 1
       for (k in 1:num_states) {
         for (l in 1:num_states) {
           if (k != l) {
@@ -55,9 +41,14 @@ fit_tpm <- function(num_states, num_subjects, num_time, beta, design) {
       for (k in 1:num_states) {
         eta_row <- sum(exp(eta[k, ]))
         for (l in 1:num_states) {
-          gamma[[i]][[t]][k, l] <- exp(eta[k, l])/eta_row
+          gamma[[i]][[k, l, t]] <- exp(eta[k, l])/eta_row
         }
       }
+    }
+  }
+  if (num_covariates == 0) {
+    for (i in num_subjects) {
+      gamma[[i]] <- gamma[[i]][, , 1]
     }
   }
   gamma
