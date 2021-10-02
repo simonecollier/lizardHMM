477aa04713c9cfa2c3a507bb5cc1ac57256abbce
Simone Collier <simonekcollier@gmail.com>
2021-10-02 17:18:18 -0400

    add print.level

--- R/gam_fit_hmm.R
+++ R/gam_fit_hmm.R
@@ -43,7 +43,7 @@ gam_fit_hmm <- function(x, design, num_states, num_variables, num_subjects,
                          num_covariates,
                          alpha0, theta0, beta0, delta0,
                          state_dep_dist_pooled = FALSE, iterlim = 100,
-                         hessian = FALSE) {
+                         hessian = FALSE, print.level = 0) {
   num_time <- nrow(x)
   working_params <- gam_working_params(num_states, num_variables, num_subjects,
                                        alpha0, theta0, beta0, delta0)
@@ -57,7 +57,8 @@ gam_fit_hmm <- function(x, design, num_states, num_variables, num_subjects,
                       num_covariates = num_covariates,
                       state_dep_dist_pooled = state_dep_dist_pooled,
                       iterlim = iterlim,
-                      hessian = hessian)
+                      hessian = hessian,
+                      print.level = print.level)
   pn    <- gam_natural_params(num_states = num_states,
                                num_variables = num_variables,
                                num_subjects = num_subjects,
